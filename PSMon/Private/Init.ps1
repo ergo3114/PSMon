a8ed91e56675dd32f13fa6b01d67f345cf49019f
Littlejohn <Cdlittlejohn1@gmail.com>
2018-04-01 14:52:35 -0400

    Initial Initialization function and testing

---
+++ PSMon/Private/Init.ps1
@@ -0,0 +1,40 @@
+function Init{
+    [cmdletbinding(SupportsShouldProcess)]
+    Param()
+
+    BEGIN{
+        Write-Verbose "[$(Get-Date)] $($MyInvocation.MyCommand) has started"
+
+        $stringConfig = @"
+<?xml version="1.0"?>
+<Configuration>
+    <Function ID="Get-PSMonFreeSpace">
+        <WarningThreshold>25</WarningThreshold>
+        <ErrorThreshold>10</ErrorThreshold>
+    </Function>
+    <Function ID="Get-PSMonProcessor">
+        <WarningThreshold>80</WarningThreshold>
+        <ErrorThreshold>90</ErrorThreshold>
+    </Function>
+    <Function ID="Get-PSMonWorkingSet">
+        <WarningThreshold>10</WarningThreshold>
+        <ErrorThreshold>20</ErrorThreshold>
+    </Function>
+</Configuration>
+"@
+    }
+
+    PROCESS{
+        Write-Verbose "Checking if configuration file exists"
+        if(!(Test-Path $PSMon.ConfigFile)){
+            Write-Verbose "Recreating configuration file"
+            if ($pscmdlet.ShouldProcess($PSMon.ConfigFile, "Recreate Config File")){
+                New-Item -Path $PSMon.ConfigFile -ItemType File -Value $stringConfig | Out-Null
+            }
+        }
+    }
+
+    END{
+        Write-Verbose "[$(Get-Date)] $($MyInvocation.MyCommand) has completed"
+    }
+}
