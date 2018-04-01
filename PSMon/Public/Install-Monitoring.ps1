c8232eb777ced1a7c89a337fd3b9e5fbce702c41
Littlejohn <Cdlittlejohn1@gmail.com>
2018-04-01 14:53:11 -0400

    Added Repair parameter to Install

--- PSMon/Public/Install-Monitoring.ps1
+++ PSMon/Public/Install-Monitoring.ps1
@@ -1,13 +1,34 @@
 ﻿function Install-Monitoring{
-    [cmdletbinding()]
+    [cmdletbinding(SupportsShouldProcess)]
     Param(
         [Parameter(Mandatory=$false)]
         [int]$PollingCycle = 15,
 
         [Parameter(Mandatory=$false)]
-        [string]$EmailAddress = $null
+        [string]$EmailAddress = $null,
+
+        [Parameter(ParameterSetName="Repair")]
+        [switch]$Repair
     )
 
+    BEGIN{
+        Write-Verbose "[$(Get-Date)] $($MyInvocation.MyCommand) has started"
+
+        if($Repair -or $(Test-Path $PSMon.ConfigFile)){
+            Write-Verbose "Running Init"
+            Init
+            Return
+        }
+    }
+
+    PROCESS{
+        
+    }
+
+    END{
+        Write-Verbose "[$(Get-Date)] $($MyInvocation.MyCommand) has completed"
+    }
     #Create Scheduled Tasks based on parameters
     #Send to email if not null
+    #repair
 }
