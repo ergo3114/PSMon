104b017f32296e5a2e6691ef6a3d66169e25a7b2
Littlejohn <Cdlittlejohn1@gmail.com>
2018-04-01 14:52:54 -0400

    Error handling for Config File

--- PSMon/Public/Get-Monitoring.ps1
+++ PSMon/Public/Get-Monitoring.ps1
@@ -12,26 +12,42 @@ function Get-Monitoring{
         $results = @{}
 
         Write-Verbose "Getting function defaults"
-        [xml]$xmlConfig = Get-Content $PSMon.ConfigFile
-        $FreeSpaceConfig = $xmlConfig.Configuration.Function | Where-Object {$_.ID -eq "Get-PSMonFreeSpace"}
-        $ProcessorConfig = $xmlConfig.Configuration.Function | Where-Object {$_.ID -eq "Get-PSMonProcessor"}
-        $WorkingSetConfig = $xmlConfig.Configuration.Function | Where-Object {$_.ID -eq "Get-PSMonWorkingSet"}
+        if(Test-Path $PSMon.ConfigFile){
+            [xml]$xmlConfig = Get-Content $PSMon.ConfigFile
+            $FreeSpaceConfig = $xmlConfig.Configuration.Function | Where-Object {$_.ID -eq "Get-PSMonFreeSpace"}
+            $ProcessorConfig = $xmlConfig.Configuration.Function | Where-Object {$_.ID -eq "Get-PSMonProcessor"}
+            $WorkingSetConfig = $xmlConfig.Configuration.Function | Where-Object {$_.ID -eq "Get-PSMonWorkingSet"}
+        } else{
+            Write-Error "Config file not found. Run 'Install-Monitoring -Repair' to correct."
+            $flag = -1
+        }
     }
 
     PROCESS{
         Write-Verbose "Getting PSMon information"
-        $FreeSpace = Get-FreeSpace -WarningThreshold $FreeSpaceConfig.WarningThreshold -ErrorThreshold $FreeSpaceConfig.ErrorThreshold
-        $Processor = Get-Processor -WarningThreshold $ProcessorConfig.WarningThreshold -ErrorThreshold $ProcessorConfig.ErrorThreshold
-        $WorkingSet = Get-WorkingSet -WarningThreshold $WorkingSetConfig.WarningThreshold -ErrorThreshold $WorkingSetConfig.ErrorThreshold
+        if([string]::IsNullOrEmpty($flag)){
+            try{
+                $FreeSpace = Get-FreeSpace -WarningThreshold $FreeSpaceConfig.WarningThreshold -ErrorThreshold $FreeSpaceConfig.ErrorThreshold
+                $Processor = Get-Processor -WarningThreshold $ProcessorConfig.WarningThreshold -ErrorThreshold $ProcessorConfig.ErrorThreshold
+                $WorkingSet = Get-WorkingSet -WarningThreshold $WorkingSetConfig.WarningThreshold -ErrorThreshold $WorkingSetConfig.ErrorThreshold
 
-        $results.Add('FreeSpace',$FreeSpace)
-        $results.Add('Processor',$Processor)
-        $results.Add('WorkingSet',$WorkingSet)
-        $results.Add('Collected',$(Get-Date))
+                $results.Add('FreeSpace',$FreeSpace)
+                $results.Add('Processor',$Processor)
+                $results.Add('WorkingSet',$WorkingSet)
+                $results.Add('Collected',$(Get-Date))
+            }
+            catch{
+                Write-Error "$($PSItem.ToString)"
+                $flag = -1
+            }
+        }
     }
 
     END{
         Write-Verbose "[$(Get-Date)] $($MyInvocation.MyCommand) has completed"
+        if($flag -lt 0){
+            return -1
+        }
         return $results
     }
 }
